b0dd8f5f99bd1d9d9b2a9a3d033786b548a0b5db
Saulius Ugintas <saulensaz@gmail.com>
2018-03-04 12:01:48 +0100

    google map distance calculation and sorting by distance

--- src/Service/GoogleMapPlacesParser.php
+++ src/Service/GoogleMapPlacesParser.php
@@ -10,12 +10,15 @@ use App\Model\VeterinaryClinic;
  */
 class GoogleMapPlacesParser
 {
-    private const API_KEY = 'AIzaSyDNlSlxiH84IdJ076Br6m-A__mAb1Gb36o';
+    private const API_KEY = 'AIzaSyDycc2ftZRn6eMC40qgUj98ESp0MlJxv7k';
     private const BASE_URL = 'https://maps.googleapis.com/maps/api/';
-    private const PLACE_SEARCH_URL = 'place/nearbysearch/json';
+//    private const PLACE_SEARCH_URL = 'place/nearbysearch/json';
+    private const PLACE_SEARCH_URL = 'place/textsearch/json';
     private const PLACE_DETAILS_URL = 'place/details/json';
+    private const DISTACE_URL = 'distancematrix/json';
     private const TYPE_VETERINARY = 'veterinary_care';
     private const RADIUS = '50000';
+    private const KEYWORD = 'veterinarijos';
 
     /**
      * @var DistanceCalculator
@@ -32,17 +35,19 @@ class GoogleMapPlacesParser
     }
 
     /**
-     * @param int $latitude
-     * @param int $longitude
+     * @param float $latitude
+     * @param float $longitude
      * @return array|null
      * @throws \Exception
      */
-    public function getVetsList(int $latitude, int $longitude): ?array
+    public function getVetsClinicsList(float $latitude, float $longitude): ?array
     {
         $queryParams = [
             'location' => $latitude . ',' . $longitude,
             'radius' => self::RADIUS,
-            'type' => self::TYPE_VETERINARY,
+//            'type' => self::TYPE_VETERINARY,
+//            'language' => 'lt',
+            'query' => self::KEYWORD,
         ];
 
         $places = $this->parseResults(self::PLACE_SEARCH_URL, $queryParams);
@@ -52,13 +57,13 @@ class GoogleMapPlacesParser
     }
 
     /**
-     * @param int $latitude
-     * @param int $longitude
+     * @param float $latitude
+     * @param float $longitude
      * @param array $results
      * @return array|null
      * @throws \Exception
      */
-    private function getFormattedResults(int $latitude, int $longitude, array $results): ?array
+    private function getFormattedResults(float $latitude, float $longitude, array $results): ?array
     {
         if (empty($results)) {
             return null;
@@ -68,7 +73,7 @@ class GoogleMapPlacesParser
         foreach ($results as $result) {
             $place = (new VeterinaryClinic())
                 ->setName(isset($result['name']) ? $result['name'] : null)
-                ->setAddress(isset($result['vicinity']) ? $result['vicinity'] : null)
+                ->setAddress(isset($result['formatted_address']) ? $result['formatted_address'] : null)
                 ->setLatitude(isset($result['geometry']['location']['lat']) ? $result['geometry']['location']['lat'] : null)
                 ->setLongitude(isset($result['geometry']['location']['lng']) ? $result['geometry']['location']['lng'] : null)
                 ->setDistance($this->calculateDistance($latitude, $longitude, $result))
@@ -83,8 +88,15 @@ class GoogleMapPlacesParser
                     ->setOpeningHours(isset($result['opening_hours']) ? $result['opening_hours'] : null);
             }
 
+//            $distance = $this->getDistanceDetails($latitude, $longitude, $result);
+//            if (!empty($distance)) {
+//                $place->setDistance($distance);
+//            }
+
             $list[] = $place;
         }
+
+        usort($list, $this->distanceCalculator->sortByKey('distance'));
 
         return $list;
     }
@@ -104,12 +116,12 @@ class GoogleMapPlacesParser
     }
 
     /**
-     * @param int $latitude
-     * @param int $longitude
+     * @param float $latitude
+     * @param float $longitude
      * @param array $result
      * @return float
      */
-    private function calculateDistance(int $latitude, int $longitude, array $result)
+    private function calculateDistance(float $latitude, float $longitude, array $result)
     {
         if (empty($result['geometry']['location'])) {
             return null;
@@ -120,6 +132,36 @@ class GoogleMapPlacesParser
         $distance = $this->distanceCalculator->distance($latitude, $longitude, $lat2, $lng2);
 
         return number_format($distance, 2);
+    }
+
+    /**
+     * @param float $latitude
+     * @param float $longitude
+     * @param array $result
+     * @return float
+     * @throws \Exception
+     */
+    private function getDistanceDetails(float $latitude, float $longitude, array $result)
+    {
+        if (empty($result)) {
+            return null;
+        }
+
+        $lat2 = $result['geometry']['location']['lat'];
+        $lng2 = $result['geometry']['location']['lng'];
+        $queryParams = [
+            'units' => 'metric',
+            'origins' => $latitude . ',' . $longitude,
+            'destinations' => $lat2 . ',' . $lng2,
+        ];
+
+        $distanceDetails = $this->parseResults(self::DISTACE_URL, $queryParams);
+
+        if (empty($distanceDetails)) {
+            return null;
+        }
+
+        return isset($distanceDetails[0]['elements']['distance']['value']) ? $distanceDetails[0]['elements']['distance']['value'] : null;
     }
 
     /**
@@ -148,6 +190,12 @@ class GoogleMapPlacesParser
             if (!empty($response['result'])) {
                 $results = $response['result'];
             }
+
+            if (!empty($response['rows'])) {
+                $results = $response['rows'];
+            }
+        } elseif (isset($response['error_message'])) {
+            throw new \Exception($response['error_message']);
         } elseif (isset($response['status'])) {
             throw new \Exception($response['status']);
         }
